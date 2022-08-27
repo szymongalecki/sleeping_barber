92fa809107635b2cba5d4a723dd480ed9d925f04
Szymon Gałecki <szymongalecki@gmail.com>
2022-08-27 22:26:40 +0200

    cleaned code and updated readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Sleeping Barber
+Barber saloon has n chairs in the waiting room and one barber chair in the barber room. Barber sleeps in the barber chair if there are no customers. If a customer enters the barber saloon and there is no one but the sleeping barber, customer wakes up the barber and sits in the barber chair. If a customer enters and barber is busy with another customer, he takes a seat in the waiting room. If a customer enters and there is no place in the waiting room, he leaves.
 
-- Client function uses select statement to make decision based on the saloon channel state.  
-- Barber function reads clients id's from the saloon channel, work is simulated with sleep.  
-- When all new clients are either inside or didn't get into the saloon, channel gets closed.  
-- Clients that are inside are serviced, range over channel is done when saloon channel is empty and closed.  
-- Barber goroutines are done -> end.  
+## Solution
+One channel is used for storing clients, and ’blocking’ them inside until barber is ready to service them. Clients are serviced in the order of coming to the saloon without additional mechanisms. There is no necessity for semaphore and mutex. If the saloon channel is full, clients will not enter. State of the saloon is known to the clients without additional variable, that would need its own critical section.
+
+## Wikipedia
+https://en.wikipedia.org/wiki/Sleeping_barber_problem
--- sb.go
+++ sb.go
@@ -4,27 +4,24 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"sync"
 	"time"
 )
 
-// Saloon capacity - (excluding the cutting chair), number of clients, number of barbers
+// saloonCapacity >= 1, clientCount >= 1, barberCount >= 1
 var saloonCapacity int = 1
 var clientCount int = 10
 var barberCount int = 1
 
-// Clients wait group, barbers wait group, saloon channel of set capacity
 var cWg sync.WaitGroup
 var bWg sync.WaitGroup
 var saloon = make(chan int, saloonCapacity)
 
-// Work simulation for output observability
+// sleep is for the output to be nicer, it is not a vital part of the algorithm
 func sleep() {
 	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 }
 
-// Barber function
 func barber(id int) {
 	defer bWg.Done()
 
@@ -34,38 +31,32 @@ func barber(id int) {
 	}
 }
 
-// Client function
 func client(id int) {
 	defer cWg.Done()
 
 	select {
 	case saloon <- id:
-		fmt.Printf("\t\t\tClient%d: enters saloon\n", id)
+		fmt.Printf("Client%d: enters saloon\n", id)
 	default:
-		fmt.Printf("\t\t\tClient%d: saloon is full, leaves\n", id)
+		fmt.Printf("Client%d: saloon is full, leaves\n", id)
 	}
 }
 
 func main() {
-
-	// Output header
-	fmt.Println("BARBER\t\t\tCLIENTS")
-	fmt.Println(strings.Repeat("- ", 30))
-
-	// Launch barber goroutines
+	// launch barber goroutines
 	for i := 0; i < barberCount; i++ {
 		bWg.Add(1)
 		go barber(i + 1)
 	}
 
-	// Launch client goroutines
+	// launch client goroutines
 	for i := 0; i < clientCount; i++ {
 		cWg.Add(1)
 		go client(i + 1)
 		sleep()
 	}
 
-	// Wait for all the client goroutines, close channel, wait for barber
+	// wait for all the client goroutines, close channel, wait for barber goroutines
 	cWg.Wait()
 	close(saloon)
 	bWg.Wait()
